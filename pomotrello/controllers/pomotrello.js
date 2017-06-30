fefc5c220f4a61eafba1018742f7060ba60915d8
Johnny Watson <johnny@streetchange.org.uk>
2017-06-30 15:56:13 +0100

    Create index route for database calls

--- pomotrello/controllers/pomotrello.js
+++ pomotrello/controllers/pomotrello.js
@@ -4,6 +4,14 @@ var pomotrelloRouter = express.Router();
 
 var Task = require('../client/src/models/task.js');
 
-var 
+var PomotrelloQuery = require('../db/pomotrello_query.js');
+var query = new PomotrelloQuery();
+
+//index 
+pomotrelloRouter.get('/', function(req, res) {
+  query.all(function(docs) {
+    res.json(docs);
+  });
+});
 
 module.exports = pomotrelloRouter;
