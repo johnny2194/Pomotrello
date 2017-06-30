499e753f978074ac8dedd498800ee55fef7a41d1
Johnny Watson <johnny@streetchange.org.uk>
2017-06-30 15:29:50 +0100

    Create seeds for tasks collection

--- pomotrello/controllers/pomotrello.js
+++ pomotrello/controllers/pomotrello.js
@@ -2,4 +2,8 @@ var express = require('express');
 var app = express();
 var pomotrelloRouter = express.Router();
 
+var Task = require('../client/src/models/task.js');
+
+var 
+
 module.exports = pomotrelloRouter;
