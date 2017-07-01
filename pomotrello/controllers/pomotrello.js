ba103e50d39ddf91c53cb09c7c39a7c0e5d9c749
Matt Rose <yamiacat@gmail.com>
2017-07-01 10:54:39 +0100

    Add create new task route to back end

--- pomotrello/controllers/pomotrello.js
+++ pomotrello/controllers/pomotrello.js
@@ -7,11 +7,37 @@ var Task = require('../client/src/models/task.js');
 var PomotrelloQuery = require('../db/pomotrello_query.js');
 var query = new PomotrelloQuery();
 
-//index 
+//index
 pomotrelloRouter.get('/', function(req, res) {
   query.all(function(docs) {
     res.json(docs);
   });
 });
+
+//add new task
+pomotrelloRouter.post("/", function(req, res) {
+  var newTask = new Task(   {
+      description: req.body.description,
+      category: req.body.category,
+      pomCount: req.body.pomCount,
+      date: req.body.date,
+      startTime: req.body.startTime,
+      endTime: req.body.endTime,
+      completed: req.body.completed
+    });
+    query.add(newTask, function(docs) {
+      res.json(docs);
+    });
+});
+
+
+
+
+
+
+
+
+
+
 
 module.exports = pomotrelloRouter;
--- pomotrello/db/pomotrello_query.js
+++ pomotrello/db/pomotrello_query.js
@@ -13,11 +13,36 @@ PomotrelloQuery.prototype = {
       collection.find().toArray( function(err, docs) {
       if(err) {
         console.log("Error in pomotrello query all - collection");
-      };    
+      };
       onQueryFinished(docs);
       })
     })
+  },
+  add: function(taskToAdd, onQueryFinished) {
+    MongoClient.connect(this.url, function(err, db) {
+      if(err){
+        console.log("Error in pomotrello query add - connect");
+      };
+      var collection = db.collection("tasks");
+      collection.insert(taskToAdd);
+      collection.find().toArray(function(err, docs) {
+          if(err) {
+            console.log("Error in pomotrello query add - collection");
+          };
+          onQueryFinished(docs);
+      });
+    });
   }
 }
+
+
+
+
+
+
+
+
+
+
 
 module.exports = PomotrelloQuery;
