add07498d5badd655be7529ee453fc81d61724ac
Matt Rose <yamiacat@gmail.com>
2017-07-05 15:42:06 +0100

    Dynamic category add new category works like a charm

--- pomotrello/client/src/views/ui.js
+++ pomotrello/client/src/views/ui.js
@@ -82,6 +82,7 @@ UI.prototype = {
           option.innerText = category;
           categorySelect.appendChild(option);
         }
+
         var addNewCategory = document.createElement("option");
         addNewCategory.innerText = "Add new category";
         categorySelect.appendChild(addNewCategory);
@@ -92,9 +93,26 @@ UI.prototype = {
               console.log("I HAVE YOU NOW");
               var addCategoryModal = document.getElementById('add-category-modal-popup');
               addCategoryModal.style.display = "block";
-              
+              var addCategoryForm = document.getElementById("add-category-form");
+              var addCategoryInput = document.getElementById("new-category-input");
+              addCategoryInput.autofocus = true;
 
+              addCategoryForm.addEventListener('submit', function (event) {
+                event.preventDefault();  //this stops redirect to new page
 
+                var newCategoryValue = addCategoryForm['new-category-input'].value;
+                console.log("newCategoryValue", newCategoryValue);
+
+                var newCategoryOption = document.createElement("option");
+                newCategoryOption.value = newCategoryValue;
+                newCategoryOption.innerText = newCategoryValue;
+                newCategoryOption.setAttribute("selected", true);
+                categorySelect.appendChild(newCategoryOption);
+
+                addCategoryModal.style.display = "none";
+
+                return false;
+              })
 
               var addCategorySpan = document.getElementById("close-add-category-modal-popup");
               // When the user clicks on <span> (x), close the modal
@@ -107,17 +125,12 @@ UI.prototype = {
                   addCategoryModal.style.display = "none";
                 }
               }
-
             }
         })
 
 
 
       });
-
-          //
-          //
-          // })
 
     }
 ///BACK TO ADD TASK MODAL POP UP
@@ -309,9 +322,6 @@ tasks.forEach(function(task) {
 
 
    })
-
-
-
 
 
 
